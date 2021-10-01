ce0a8e118534a109bce174756d42fed0927d2fe5
JulioPazzaglia <julitopazzaglia@gmail.com>
2021-10-01 13:15:30 -0300

    reset

--- back/config/db.js
+++
@@ -1,6 +0,0 @@
-const Sequelize = require("sequelize")
-
-const { options, database } = require("./dbsettings.json")
-const db = new Sequelize(database, null, null, options)
-
-module.exports = db
--- back/models/index.js
+++
@@ -1,7 +0,0 @@
-const Listas = require("./listas");
-const Personas = require("./personas");
-
-Personas.belongsToMany(Listas, { through: "listas_personas" });
-Listas.belongsToMany(Personas, { through: "listas_personas" });
-
-module.exports = { Listas, Personas };
